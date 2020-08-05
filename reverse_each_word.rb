fba861e5406fbd36bf57e0e0fd3225e861328a7f
Javier Aguilar <coyote176@msn.com>
2020-08-05 21:47:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(sentence)
 
- 
+ original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
 # describe '#reverse_each_word' do
  # let(:sentence1) { "Hello there, and how are you?" }
   #it 'reverses all the words in a string without reversing the order of the words' do
